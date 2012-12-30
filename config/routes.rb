b606cfd6aa9b16858a81715fc5734670a7329d12
Aaron Hubbard <aaron@aaron-laptop.(none)>
2012-12-30 13:12:33 -0500

    got run section working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
+ 
+  resources :runs
+
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'runs#index'
+    resources :runs
   end
-  root :to => "home#index"
+  
+  root :to => "users#index"
   devise_for :users
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121229172053) do
+ActiveRecord::Schema.define(:version => 20121230142858) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(:version => 20121229172053) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
+
+  create_table "runs", :force => true do |t|
+    t.date     "date"
+    t.float    "distance"
+    t.integer  "hours"
+    t.integer  "minutes"
+    t.integer  "seconds"
+    t.text     "notes"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "runs", ["user_id"], :name => "index_runs_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
